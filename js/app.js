08c4303b729e212285c5c4ef35b72da1fb04a435
Jeffrey Grinberg <grinbergjeff@gmail.com>
2015-09-02 19:47:08 -0400

    implementing make active function

--- js/app.js
+++ js/app.js
@@ -4,4 +4,22 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	
 	//Hide landing page for now to implement design for other areas
 	document.getElementById('intro-section').style.display='none';
+	
+	makeActive();
 });
+
+/*Functions*/
+
+function makeActive(clickedButton) {
+	//var clickedButton = document.getElementById('menu-p');
+	clickedButton.addEventListener("click", function() {
+		var activeElement = document.getElementsByClassName('active');
+			alert('yy');
+		for (var i = 0; i < activeElement.length; i++) {
+			activeElement[i].setAttribute('class', '');
+			clickedButton.setAttribute('class','active');
+		}
+		//document.getElementsByClassName('active').className.replace('');
+		//document.getElementById('menu-p').className.replace('active');
+	});
+}
