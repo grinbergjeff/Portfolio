344076e6cb1310a3dc9dbd8caa652172e71bf925
Jeffrey Grinberg <grinbergjeff@gmail.com>
2015-09-02 00:18:43 -0400

    implemented left arrow to go back to earlier section

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 document.addEventListener("DOMContentLoaded", function(event) { 
 	//Hide abot Me section for now
-	document.getElementById('about-me').style.display='none';
+	/*document.getElementById('about-me').style.display='none';*/
+	
+	//Hide landing page for now to implement design for other areas
+	document.getElementById('intro-section').style.display='none';
 });
